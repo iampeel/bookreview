26ba42018f481de52e2a187d9dfb27b29e85062e
moonpil cheon <iampeel@naver.com>
2022-01-04 18:57:04 +0900

    리뷰 저장하기

--- venvbook/app.py
+++ venvbook/app.py
@@ -2,6 +2,7 @@ from pymongo import MongoClient
 from flask import Flask, render_template, jsonify, request
 app = Flask(__name__)
 
+# db에 dbsparta라는 폴더 만들기(값이 들어가야 생김)
 client = MongoClient('localhost', 27017)
 db = client.dbsparta
 
@@ -13,13 +14,34 @@ def home():
 
 
 # API 역할을 하는 부분
+# 리뷰 저장하기
 @app.route('/review', methods=['POST'])
 def write_review():
-    sample_receive = request.form['sample_give']
-    print(sample_receive)
+    # 리뷰 저장하기
+    # index.html의 makeRiview()로부터 입력된 data 받아오기
+    title_receive = request.form['title_give']
+    author_receive = request.form['author_give']
+    review_receive = request.form['review_give']
+
+    # 리뷰 저장하기
+    # db에 데이터 공간 만들기
+    doc = {
+        'title': title_receive,
+        'author': author_receive,
+        'review': review_receive
+    }
+
+    # 리뷰 저장하기
+    # db의 bookreview폴더에 데이터 저장
+    db.bookreview.insert_one(doc)
+
+    # 리뷰 저장하기
+    # 완료되면 나오는 alert메세지
     return jsonify({'msg': '이 요청은 POST!'})
 
 
+# API 역할을 하는 부분
+# 리뷰 보여주기
 @app.route('/review', methods=['GET'])
 def read_reviews():
     sample_receive = request.args.get('sample_give')
